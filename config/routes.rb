ca4d96c6ac189c23cc7c10ca18aae6d3ee4bc816
nozomi takada <g031n096@s.iwate-pu.ac.jp>
2018-11-29 10:09:42 +0000

    first

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get '/tops', to:'tops#index'
+  root "tops#index"
   
+  resources :tops do
+    collection do
+      post :blogs
+    end
+  end
   
   resources :blogs do
     collection do
       post :confirm
     end
   end
-
- 
 end
