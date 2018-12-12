2ac1de1acd3f8f5b810388964bec41dac5898c4f
nozomi takada <g031n096@s.iwate-pu.ac.jp>
2018-12-12 09:51:34 +0000

    init

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,7 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
     @blogs = Blog.all
+    #@user = User.find(user_id)
     @favorites = Favorite.all
   end
   
