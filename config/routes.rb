9ff8517bb70fde72e9dc63372483bcece3c1fefb
nozomi takada <g031n096@s.iwate-pu.ac.jp>
2018-12-18 14:02:32 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     end
     resources :contacts
     #mount LetterOpenerWeb::Engine, at:"/letter_opener" if Rails.env.development?
-    if Rails.env.development? || Rails.env.production?
-        mount LetterOpenerWeb::Engine, at:"/letter_opener"
-    end
+##    if Rails.env.development? || Rails.env.production?
+##        mount LetterOpenerWeb::Engine, at:"/letter_opener"
+##    end
 end
