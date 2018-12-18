7518b3a23ed3a5c79d4e04e3e30af33f457a9e6b
nozomi takada <g031n096@s.iwate-pu.ac.jp>
2018-12-18 06:44:43 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,23 @@
 Rails.application.routes.draw do
-  root to: "tops#index"
-  
-   #get 'sessions/new'
-   
- resources :favorites, only:[:create, :destroy]
- resources :sessions, only: [:new, :create, :show, :destroy]
- resources :users, only:[:new, :show, :create]
- resources :tops do
-   collection do
-     get :tops
-   end
- end
- resources :new do
-   collection do
-     post :new
-   end
- end
- resources :blogs do
-    collection do
-        post :confirm
+    root to: "tops#index"
+    resources :favorites, only:[:create, :destroy]
+    resources :sessions, only: [:new, :create, :show, :destroy]
+    resources :users, only:[:new, :show, :create]
+    resources :tops do
+        collection do
+            get :tops
         end
- end
- resources :contacts
- 
-mount LetterOpenerWeb::Engine, at:"/letter_opener"if Rails.env.development?
-
+    end
+    resources :new do
+        collection do
+            post :new
+        end
+    end
+    resources :blogs do
+        collection do
+            post :confirm
+        end
+    end
+    resources :contacts
+    mount LetterOpenerWeb::Engine, at:"/letter_opener" if Rails.env.development?
 end
