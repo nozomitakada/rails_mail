3e6d3e69f0b43d327f55ec0ed3ffebe3085f28c4
nozomi takada <g031n096@s.iwate-pu.ac.jp>
2018-12-21 08:53:42 +0000

    init

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,7 +20,7 @@ class BlogsController < ApplicationController
     
     if @blog.save
       BlogMailer.blog_mail(@blog).deliver
-      redirect_to blogs_path, notice: "ブログを作成しました！"
+      redirect_to @blog, notice: "ブログを作成しました！"
     else
       render 'new'
     end
