0bd8fff52d5159140e6746fc06554b4c6fd19a3e
nozomi takada <g031n096@s.iwate-pu.ac.jp>
2018-12-22 11:39:00 +0000

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,23 +14,24 @@ class UsersController < ApplicationController
     def show
        #params[:id]
        @user = User.find(params[:id])
-       @feeds = Feed.all
+       #@feeds = Feed.all
        #@favorites_blogs = current_user.favorite_blogs
        #@favorites_blogs = @user.favorites
     end
     
-    def new_img
-        if params[:back]
-          @user_img = Feed.new(feed_params)
-        else
-          @user_img = Feed.new
-        end
-    end
+#    def new_img
+#        if params[:back]
+#          @user_img = Feed.new(feed_params)
+#        else
+#          @user_img = Feed.new
+#        end
+#    end
     
     private
     
     def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
+        #params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
 end
